42cdfff17c956df1f9d3f5b4677e8f637d9a7c40
Joaquín Jiménez García <garcia.jjimenez@gmail.com>
2018-09-28 01:05:30 +0200

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="login">
+        <h3>Accede</h3>
+        <input v-model="email" type="email" placeholder="Email"><br>
+        <input v-model="password" type="password" placeholder="Password"><br>
+        <button v-on:click="signIn">Conectar</button>
+        <p>¿No tienes cuenta? <router-link to="/sign-up"> Regístrate ahora.</router-link></p>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+    name: 'login',
+    data: function() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        signIn: function() {
+            firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+                (user) => {
+                    this.$router.replace('hello')
+                },
+                (err) => {
+                    alert('Oops. ' + err.message)
+                }
+            );
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .login {
+        margin-top: 40px;
+    }
+    input{
+        margin: 10px 0;
+        width: 20%;
+        padding: 15px;
+    }
+    button{
+        margin-top: 20px;
+        width: 10%;
+        cursor: pointer;
+    }
+    p{
+        margin-top: 40px;
+        font-size: 13px;
+    }
+    p a{
+        text-decoration: underline;
+        cursor: pointer;
+    }
+</style>
+
+
---
+++ src/main.js
@@ -0,0 +1,33 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import firebase from 'firebase'
+
+Vue.config.productionTip = false
+
+let app;
+
+// Declaration of Firebase
+var config = {
+  apiKey: "AIzaSyBwJHIxKFq0Ep_1HgHRrVDHEbDJeN8t5ys",
+  authDomain: "pruebavue-7d509.firebaseapp.com",
+  databaseURL: "https://pruebavue-7d509.firebaseio.com",
+  projectId: "pruebavue-7d509",
+  storageBucket: "pruebavue-7d509.appspot.com",
+  messagingSenderId: "856743667637"
+};
+
+firebase.initializeApp(config);
+firebase.auth().onAuthStateChanged(function (user) {
+  if (!app) {
+    /* eslint-disable no-new */
+    new Vue({
+      el: '#app',
+      router,
+      components: { App },
+      template: '<App/>'
+    })
+  }
+});
