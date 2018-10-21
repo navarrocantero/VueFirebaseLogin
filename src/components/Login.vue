6b9852819d608a5b2818c890134af87a88d4f51e
joseantonio@secmotic.com <joseantonio@secmotic.com>
2018-10-21 12:20:55 +0200

    2pr

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,61 +1,67 @@
 <template>
-    <div class="login">
-        <h3>LOGIN</h3>
-        <input v-model="email" type="email" placeholder="Email"><br>
-        <input v-model="password" type="password" placeholder="Password"><br>
-        <button v-on:click="signIn">Conectar</button>
-        <p>¿NO account? <router-link to="/sign-up"> Regístrate ahora.</router-link></p>
-    </div>
+  <div class="login">
+    <h3>LOGIN</h3>
+    <input v-model="email" type="email" placeholder="Email"><br>
+    <input v-model="password" type="password" placeholder="Password"><br>
+    <button v-on:click="signIn">Conectar</button>
+    <p>¿NO account?
+      <router-link to="/sign-up"> Regístrate ahora.</router-link>
+    </p>
+  </div>
 </template>
 
 <script>
-import firebase from 'firebase'
+  import firebase from 'firebase'
 
-export default {
+  export default {
     name: 'login',
-    data: function() {
-        return {
-            email: '',
-            password: ''
-        }
+    data: function () {
+      return {
+        email: '',
+        password: ''
+      }
     },
     methods: {
-        signIn: function() {
-            firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
-                (user) => {
-                    this.$router.replace('hello')
-                },
-                (err) => {
-                    alert('Oops. ' + err.message)
-                }
-            );
-        }
+      signIn: function () {
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+          (user) => {
+            this.$router.replace('hello')
+          },
+          (err) => {
+            alert('Oops. ' + err.message)
+          }
+        );
+      }
     }
-}
+  }
 </script>
 
 <style scoped>
-    .login {
-        margin-top: 40px;
-    }
-    input{
-        margin: 10px 0;
-        width: 20%;
-        padding: 15px;
-    }
-    button{
-        margin-top: 20px;
-        width: 10%;
-        cursor: pointer;
-    }
-    p{
-        margin-top: 40px;
-        font-size: 13px;
-    }
-    p a{
-        text-decoration: underline;
-        cursor: pointer;
-    }
+  .login {
+    margin-top: 40px;
+  }
+
+  input {
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+  }
+
+  button {
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+  }
+
+  p {
+    margin-top: 40px;
+    font-size: 13px;
+  }
+
+  p a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
 </style>
 
 
